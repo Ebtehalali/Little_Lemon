22cdc297f90cd3f49c17fd575f43146daf1760f6
Ibtehal Ali <ibtehalali22@gmail.com>
2023-07-22 17:19:36 +0300

    created header and main sections for little lemon app

--- little-lemon/src/Component/Main.js
+++ little-lemon/src/Component/Main.js
@@ -2,7 +2,23 @@ import restauranfood from '../Images/restauranfood.jpg';
 
 function Main() {
     return(
-        <></>
+        <>
+            <main className="main">
+                <div className="container">
+                    <div className="left-side">
+                        <h1>Little Lemon</h1>
+                        <h2>Chicago</h2>
+                        <p>
+                        We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.
+                        </p>
+                        <button>Reserve a Table</button>
+                    </div>
+                    <div className="right-side">
+                        <img src={restauranfood} alt="restauranfood" />
+                    </div>
+                </div>
+            </main>
+        </>
     )
 }
 
